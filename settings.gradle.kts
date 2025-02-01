ddb3e5a92cb67704de96840d2326556bcde4b01f
Divyansh Nigam <124130586+Divyansh121699@users.noreply.github.com>
2025-02-01 14:38:36 -0500

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        //maven { url = uri("https://repo.hivemq.com/maven/") } // Add HiveMQ repository
+        //maven { url = uri("https://repo.hivemq.com/maven/") }
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DoctorApp2"
+include(":app")
+ 
