278762a042a63e96dba723ceea1c5129f10e0b09
Divyansh Nigam <124130586+Divyansh121699@users.noreply.github.com>
2025-02-01 15:33:28 -0500

    Update mqtt_config.py

--- Project-simulation/data generation/mqtt_config.py
+++ Project-simulation/data generation/mqtt_config.py
@@ -6,5 +6,5 @@ class mqttConfig:
     MQTT_BROKER = "6bc0e53bbb904551a9d0a88725cd9fdb.s1.eu.hivemq.cloud"
     MQTT_PORT = 8883
     MQTT_USERNAME = "Divyansh16"
-    MQTT_PASSWORD = "Divyans12@"
+    MQTT_PASSWORD = "**********"
     TOPIC = "healthcare/sensor_data"
